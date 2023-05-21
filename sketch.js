2d1be07bba7f62b77b65c2a8b8b30c30e1dc47e9
Angel Cardenas <cardenasangel961@gmail.com>
2023-05-21 01:52:44 -0700

    Adding fotos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,116 @@
-function setup(){
-    background(125);
+let fondoJuego
+let fondoInicio
+let x = 0
+let posY = 50
+let dY = 3
+let estado = 0
+let record = 0
+let wallX = [600, 900]
+let wallY = [400, 600]
+let recordAnterior = 0
+let puntaje = 0
+let musicaRecord
+let musicaJuego
+let imagenPersonaje
+let imagenPared
+
+function preload() { 
+  fondoJuego = loadImage('./images/fondojuego.png')
+  fondoInicio = loadImage('./images/fondoinicio.png')
+  musicaRecord = loadSound('./sounds/aplauso.wav')
+  musicaJuego = loadSound('./sounds/boombayah.mp3')
+  imagenPersonaje = loadImage('./images/bird.png')
+  imagenPared = loadImage('./images/pared.png')
 }
 
-function draw(){
-    triangle(30, 75, 58, 20, 86, 75);
+function setup() {
+  // put setup code here
+  createCanvas(1200,800)
+  textSize(34)
+}
+
+function draw() {
+  // put drawing code here
+  //background(0)
+  //square(150,150,100)
+  if (estado === 1) {
+    imageMode(CORNER)
+    image(fondoJuego,x,0)
+    image(fondoJuego,x+fondoJuego.width,0)
+    x = x - 6
+    //Movimiento del personaje
+    dY = dY + 1  //dy += 1
+    posY = posY + dY
+    //obstaculos
+    for (let i = 0; i < wallX.length; i++) {
+      //Obstáculos
+      imageMode(CENTER)
+      image(imagenPared,wallX[i],wallY[i]-500)
+      image(imagenPared,wallX[i],wallY[i]+500)
+
+      //Generar nuevas coordenadas para las paredes
+      if (wallX[i] < 0) {
+        wallX[i] = width
+        wallY[i] = random(200, 600)
+      }
+      //Agregar puntaje
+      if (wallX[i] === 300) {
+        puntaje = puntaje + 1
+        record = max(puntaje, record)
+      }
+
+      //Colisiones
+      if (posY < 0 || posY > height || (abs(wallX[i]-300) < 50 && abs(posY - wallY[i])  > 100)) {
+        estado  = 0
+        musicaJuego.stop()
+        cursor()
+      }
+
+      wallX[i] += -6 //Desplazamiento de las paredes
+    }
+
+
+
+    if (x < -fondoJuego.width) {
+      x = 0
+    }
+    //Desplegamos al personaje
+    //text("👻",300,posY)
+    image(imagenPersonaje,300,posY,50,50)
+    //Desplegamos el puntaje
+    text("Puntaje: " + puntaje, width/2-50, 50)
+
+  } else {  //estado = 0
+    //imageMode(CENTER)
+    imageMode(CORNER)
+    background(0)
+    image(fondoInicio,0,0)
+    text("Record: " + record, 60, 450)
+    if (recordAnterior != record) {
+      if (!musicaRecord.isPlaying()) {
+        musicaRecord.play()
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  if (estado === 0) {
+    estado = 1
+    recordAnterior = record
+    puntaje = 0
+    x = 0
+    posY = 50
+    dY = 3
+    wallX = [600, 900]
+    wallY = [400, 600] 
+    noCursor()  
+    if (musicaRecord.isPlaying()) {
+      musicaRecord.stop()
+    }
+    //musicaJuego.play()
+
+  } else {
+    dY = -15
+  }
 }
