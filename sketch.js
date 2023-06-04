81d77ca43f0b51b66294e736a0ee5e3060f22da6
Angel Cardenas <cardenasangel961@gmail.com>
2023-06-04 10:50:15 -0700

    finaly added the Menu

--- sketch.js
+++ sketch.js
@@ -2,9 +2,20 @@
 let backgroundGame;
 let backgroundMenu;
 let imageWall;
+//Animation
 let imageMario, imageLuigi;
 let imageFMario, imageFLuigi;
 let imageTMario, imageTLuigi;
+let imageCharacter, imageFalling, imageTransition;
+//Menu
+let selectMario, selectLuigi;
+let happyMario, happyLuigi;
+let widthNMario = 40,
+  widthNLuigi = 40;
+let heightNMario = 85,
+  heightNLuigi = 85;
+let im_c = 0;
+
 let wallX = [600, 900, 1200, 1500];
 let wallY = [400, 600, 500, 300];
 let myFont;
@@ -28,9 +39,16 @@ function preload() {
   backgroundGame = loadImage("./img/Backgrounds/overworld.jpg");
   backgroundMenu = loadImage("./img/Backgrounds/castle.jpg");
   imageWall = loadImage("./img/pared.png");
-  imageCharacter = loadImage("./img/characters/mario.png");
-  imageFalling = loadImage("./img/characters/marioFalling.png");
-  imageTransition = loadImage("./img/characters/marioTFalling.png");
+  imageMario = loadImage("./img/characters/mario.png");
+  imageFMario = loadImage("./img/characters/marioFalling.png");
+  imageTMario = loadImage("./img/characters/marioTFalling.png");
+  imageLuigi = loadImage("./img/characters/luigi.png");
+  imageFLuigi = loadImage("./img/characters/luigiFalling.png");
+  imageTLuigi = loadImage("./img/characters/luigiTFalling.png");
+  selectMario = loadImage("./img/characters/selectionCharacterMario.png");
+  happyMario = loadImage("./img/characters/HappyMario.png");
+  selectLuigi = loadImage("./img/characters/selectionCharacterLuigi.png");
+  happyLuigi = loadImage("./img/characters/happyLuigi.png");
   myFont = loadFont("./fond/SuperMario.ttf");
   coin = loadSound("./sounds/coin.wav");
 }
@@ -109,9 +127,79 @@ function draw() {
     text("score: " + score, width / 2 - 50, 50);
     // coin.play();
   } else {
+    //Estado == 0
     imageMode(CORNER);
     image(backgroundMenu, 0, 0, backgroundMenu.width, 800);
     text("Record: " + record, 100, 450);
+    if (
+      mouseX > 575 &&
+      mouseX < 575 + widthNMario &&
+      mouseY > height / 3 + 50 &&
+      mouseY < height / 3 + 50 + heightNMario
+    ) {
+      //This is the hover effect
+      if (im_c == 0) {
+        image(happyMario, 575, height / 3 + 50, 50, 95);
+      } else {
+        image(selectMario, 575, height / 3 + 50, 50, 95);
+      }
+    } else if (im_c == 0) {
+      imageCharacter = imageMario;
+      imageFalling = imageFMario;
+      imageTransition = imageTMario;
+      image(happyMario, 575, height / 3 + 50, 50, 95);
+    } else {
+      if (im_c == 0) {
+        image(happyMario, 575, height / 3 + 50, 50, 95);
+      } else {
+        image(selectMario, 575, height / 3 + 50, 40, 85);
+      }
+    }
+
+    if (
+      mouseX > 725 &&
+      mouseX < 725 + widthNLuigi &&
+      mouseY > height / 3 + 50 &&
+      mouseY < height / 3 + 50 + heightNLuigi
+    ) {
+      if (im_c == 1) {
+        //This is the hover effect
+        image(happyLuigi, 725, height / 3 + 50, 60, 105);
+      } else {
+        image(selectLuigi, 725, height / 3 + 50, 60, 105);
+      }
+    } else if (im_c == 1) {
+      imageCharacter = imageLuigi;
+      imageFalling = imageFLuigi;
+      imageTransition = imageTLuigi;
+      image(happyLuigi, 725, height / 3 + 50, 60, 105);
+    } else {
+      image(selectLuigi, 725, height / 3 + 50, 50, 85);
+    }
+    selCharacter();
+    text("Mario", 550, height / 3 + 50);
+    text("Luigi", 710, height / 3 + 50);
+  }
+}
+function selCharacter() {
+  if (
+    mouseX > 575 &&
+    mouseX < 575 + widthNMario &&
+    mouseY > height / 3 + 50 &&
+    mouseY < height / 3 + 50 + heightNMario &&
+    mouseIsPressed
+  ) {
+    // 0 = Mario
+    im_c = 0;
+  } else if (
+    mouseX > 725 &&
+    mouseX < 725 + widthNLuigi &&
+    mouseY > height / 3 + 50 &&
+    mouseY < height / 3 + 50 + heightNLuigi &&
+    mouseIsPressed
+  ) {
+    // 1 = Luigi
+    im_c = 1;
   }
 }
 
